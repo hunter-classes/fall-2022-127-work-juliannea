e75b0cfbfeff06c25b9172b26e1daf9d69a5c375
juliannea1 <112399053+juliannea@users.noreply.github.com>
2022-09-02 16:57:53 +0000

    updated chapter 2

---
+++ turtles/tl.py
@@ -0,0 +1,23 @@
+import turtle
+
+wn = turtle.Screen()
+
+crush = turtle.Turtle()
+
+crush.forward(50)
+
+crush.left(50)
+
+crush.forward(50)
+
+crush.left(30)
+
+crush.forward(130)
+
+crush.right(50)
+
+crush.forward(70)
+
+wn.exitonclick()  ##exits the program when click 
+
+wn.mainloop() ##just have to put for turtle
