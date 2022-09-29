bc2fdaf12fc9cf38b222b57f84eae0e7a34028a1
juliannea <112399053+juliannea@users.noreply.github.com>
2022-09-28 21:08:31 -0400

    Update fizzbuzz.py

--- piglatin/fizzbuzz.py
+++ piglatin/fizzbuzz.py
@@ -1,3 +1,4 @@
+
 for i in range(101):
     if i % 3 == 0 and i % 5 == 0:
         print('fizzbuzz')
@@ -7,7 +8,10 @@ for i in range(101):
         
     elif i % 5 ==0:
         print('buzz')
-    
-    
-    print(i)
+        
+    elif i % 5 !=0:
+        print(i)
+        
+    elif i % 3 !=0:
+        print(i)
     
